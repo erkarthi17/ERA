2b73e8dd9870d849fde99af68f40b67af00decea
Karthick Kannan <erkarthi17@gmail.com>
2025-10-22 14:02:07 +0100

    Update download_imagenet_to_s3.py to use environment variable for Hugging Face token
    - Modified the script to retrieve the Hugging Face token from an environment variable instead of hardcoding it.
    - Ensured compatibility with the latest Hugging Face datasets API by updating the token parameter in the dataset loading function.

--- ResNet_50_Mini_Capstone/EC2_Upload_ImageNet1K/download_imagenet_to_s3.py
+++ ResNet_50_Mini_Capstone/EC2_Upload_ImageNet1K/download_imagenet_to_s3.py
@@ -2,6 +2,10 @@ from datasets import load_dataset
 import boto3, io, os
 from PIL import Image
 from tqdm import tqdm
+import os
+
+HF_TOKEN = os.getenv("HF_TOKEN", "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") 
+
 
 HF_DATASET = "ILSVRC/imagenet-1k"
 S3_BUCKET  = "imagenet-dataset-karthick-kannan"
@@ -11,7 +15,7 @@ SPLITS     = ["train", "validation"]
 s3 = boto3.client("s3")
 
 for split in SPLITS:
-    ds = load_dataset(HF_DATASET, split=split, streaming=True, use_auth_token=True)
+    ds = load_dataset(HF_DATASET, split=split, streaming=True, token=HF_TOKEN)
     print(f"Uploading {split} split...")
 
     for i, ex in enumerate(tqdm(ds, total=None)):
