39f45454a3f6ea6541fdc74b86e18a0b240516ef
Karthick Kannan <erkarthi17@gmail.com>
2025-10-22 13:51:56 +0100

    Add script to upload ImageNet dataset to S3
    - Created download_imagenet_to_s3.py to facilitate uploading ImageNet dataset splits to an S3 bucket.
    - Utilized Hugging Face datasets for streaming and processing images.
    - Implemented progress tracking with tqdm during the upload process.

---
+++ ResNet_50_Mini_Capstone/EC2_Upload_ImageNet1K/download_imagenet_to_s3.py
@@ -0,0 +1,27 @@
+from datasets import load_dataset
+import boto3, io, os
+from PIL import Image
+from tqdm import tqdm
+
+HF_DATASET = "ILSVRC/imagenet-1k"
+S3_BUCKET  = "imagenet-dataset-karthick-kannan"
+S3_PREFIX  = "imagenet1k"
+SPLITS     = ["train", "validation"]
+
+s3 = boto3.client("s3")
+
+for split in SPLITS:
+    ds = load_dataset(HF_DATASET, split=split, streaming=True, use_auth_token=True)
+    print(f"Uploading {split} split...")
+
+    for i, ex in enumerate(tqdm(ds, total=None)):
+        label = ex["label"]
+        img   = ex["image"]
+        img_bytes = io.BytesIO()
+        img.save(img_bytes, format="JPEG", quality=90)
+        img_bytes.seek(0)
+
+        key = f"{S3_PREFIX}/{split}/{label}/{i:07d}.jpg"
+        s3.upload_fileobj(img_bytes, S3_BUCKET, key)
+
+print("✅ Upload complete.")
