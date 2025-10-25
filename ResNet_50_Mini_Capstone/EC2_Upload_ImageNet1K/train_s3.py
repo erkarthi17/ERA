a9e2cb9edfa739c427646f35e2decf9396d00675
Karthick Kannan <erkarthi17@gmail.com>
2025-10-25 15:51:14 +0100

    Fix import statement in train_s3.py to correctly reference matplotlib.pyplot

--- ResNet_50_Mini_Capstone/EC2_Upload_ImageNet1K/train_s3.py
+++ ResNet_50_Mini_Capstone/EC2_Upload_ImageNet1K/train_s3.py
@@ -8,7 +8,7 @@ import sys
 import time
 from pathlib import Path
 from tqdm import tqdm
-import matlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # Import our modules from the local directory
 from .config import Config
